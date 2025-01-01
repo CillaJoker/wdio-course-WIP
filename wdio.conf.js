d23f662f1201ea26f1a4c0533500f681a0821a7d
Charles Gill Jr <charliegilljr@gmail.com>
2025-01-01 13:37:52 -0500

    npm updates and run locally

--- wdio.conf.js
+++ wdio.conf.js
@@ -77,7 +77,7 @@ export const config = {
     capabilities: [{
         // capabilities for local browser web tests
         maxInstances: 5,
-        browserName: 'chrome' // or "firefox", "microsoftedge", "safari"
+        browserName: 'firefox' // or "firefox", "microsoftedge", "safari"
     },
 ],
 
@@ -131,7 +131,7 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services : ['selenium-standalone'],
+    //services : ['selenium-standalone'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
